9f95860e28cff35f9efbe20126af2023af00d653
김 동빈 <37425087+mdongbin@users.noreply.github.com>
2020-05-09 22:25:09 +0900

    init

---
+++ app/src/main/java/com/example/tracingmvvm/ContactViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.tracingmvvm
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+
+class ContactViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = ContactRepository(application)
+    private val contacts = repository.getAll()
+
+    fun getAll(): LiveData<List<Contact>> {
+        return this.contacts
+    }
+
+    fun insert(contact: Contact) {
+        repository.insert(contact)
+    }
+
+    fun delete(contact: Contact) {
+        repository.delete(contact)
+    }
+}
--- app/src/main/java/com/example/tracingmvvm/MainActivity.kt
+++ app/src/main/java/com/example/tracingmvvm/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.example.tracingmvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var contactViewModel: ContactViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set contactItemClick & contactItemLongClick lambda
+        val adapter = ContactAdapter({ contact ->
+            val intent = Intent(this, AddActivity::class.java)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_NAME, contact.name)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_NUMBER, contact.number)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_ID, contact.id)
+            startActivity(intent)
+        }, { contact ->
+            deleteDialog(contact)
+        })
+
+        val lm = LinearLayoutManager(this)
+
+        main_recyclerview.adapter = adapter
+        main_recyclerview.layoutManager = lm
+        main_recyclerview.setHasFixedSize(true)
+
+        main_button.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun deleteDialog(contact: Contact) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Delete selected contact?")
+            .setNegativeButton("NO") { _, _ -> }
+            .setPositiveButton("YES") { _, _ ->
+                contactViewModel.delete(contact)
+            }
+        builder.show()
     }
 }
+
